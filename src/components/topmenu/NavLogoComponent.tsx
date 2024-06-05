cd7f69a1551ca7e5d236e44b96054a79a57ec329
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-05 11:56:41 -0600

    Add staticwebapp.config.json file
    A new configuration file, staticwebapp.config.json, has been added. This file is intended to manage the routing in our static web app, specifically it has been configured to direct all routes to index.html with a status code of 200.

--- src/components/topmenu/NavLogoComponent.tsx
+++ src/components/topmenu/NavLogoComponent.tsx
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import styled from 'styled-components';
+import logo from '../../assets/Asset 3.png';
 
 const StyledLink = styled(Link)`
     font-size: 1.5em;
@@ -12,14 +13,20 @@ const StyledLink = styled(Link)`
     &:hover {
         color: #f0f0f0;
     }
-    
+
     @media (max-width: 600px) {
         font-size: 1em;
+    }
+
+    img {
+        cursor: pointer;
     }
 `;
 
 export const NavLogoComponent = () => {
     return (
-        <StyledLink to="/">Crew Finding</StyledLink>
+        <StyledLink to="/">
+            <img src={logo} alt="Logo" width="20px" />
+        </StyledLink>
     );
 };
