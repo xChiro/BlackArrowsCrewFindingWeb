3f815692ba11371296520506eeb842edfc365e24
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-05 10:57:10 -0600

    Integrated useAuthToken into useGetCrew hook
    The useGetCrew hook has been modified to use the useAuthToken hook. This alteration facilitates fetching the crew details by adding token-based authentication. In the event of an error while obtaining the token or crew details, the error state is set to the encountered error and loading state is set to false.

--- src/hooks/crews/useGetCrew.tsx
+++ src/hooks/crews/useGetCrew.tsx
@@ -1,26 +1,33 @@
 import {useEffect, useState} from "react";
 import CrewService from "../../services/CrewService.ts";
 import {Crew} from "../../services/models/crews/Crew.ts";
+import {useAuthToken} from "../useAuthToken.tsx";
 
 const useGetCrew = (id: string) => {
     const [crew, setCrew] = useState<Crew | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<Error | null>(null);
+    const getToken = useAuthToken();
 
     useEffect(() => {
         setLoading(true);
-        new CrewService().getCrew(id).then(
-            crew => {
-                setCrew(crew);
-            }
-        ).catch(
-            error => {
-                setError(error);
-            }).finally(
-            () => {
-                setLoading(false);
-            }
-        );
+        getToken().then(token => {
+            new CrewService(token).getCrew(id).then(
+                crew => {
+                    setCrew(crew);
+                }
+            ).catch(
+                error => {
+                    setError(error);
+                }).finally(
+                () => {
+                    setLoading(false);
+                });
+        }).catch(error => {
+            setError(error);
+        }).finally(() => {
+            setLoading(false);
+        });
     }, [id]);
 
     return {crew, error, isLoading: loading};
