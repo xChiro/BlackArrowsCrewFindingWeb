91c0f994acadb1d295481335f447d9e7f90c36e6
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-08 23:46:00 -0600

    Add user authentication to CrewCardCreateButton component
    The CrewCardCreateButton component has been modified to incorporate user authentication. Now before redirecting to the '/crews/create' page, it checks whether the user profile is loaded or not. If not, it initiates an Auth0 login redirection.

--- src/components/crews/cards/CrewCardCreateButton.tsx
+++ src/components/crews/cards/CrewCardCreateButton.tsx
@@ -1,15 +1,23 @@
 import {StyledCardButton} from "../../utilities/cards/StyledCardButton.tsx";
 import {useNavigate} from "react-router-dom";
 import {colors} from "../../../themes/Colors.ts";
+import {useAuth0} from "@auth0/auth0-react";
+import {usePlayer} from "../../../hooks/usePlayerProfile.tsx";
 
 const CrewCardCreateButton = () => {
     const navigate = useNavigate();
+    const {loginWithRedirect} = useAuth0();
+    const {isProfileLoaded} = usePlayer();
 
     const onClick = async () => {
         try {
-            navigate('/crews/create');
-        }
-        catch (e) {
+            if (isProfileLoaded()) {
+                navigate('/crews/create');
+            } else {
+                await loginWithRedirect();
+            }
+        } catch
+            (e) {
             console.error(e);
         }
     }
