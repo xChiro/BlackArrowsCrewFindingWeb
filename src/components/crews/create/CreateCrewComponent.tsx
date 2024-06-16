89539dea30d393c932c6a7633f5ebf4b7e1fd28b
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-16 13:36:20 -0600

    Implement nested location selection in CreateCrewComponent
    Improved the location selection in the crew creation form to effectively handle changes based on the selected system, planetary system, and moon. Reorganized location fields to enable nested selection, enhancing user experience and accuracy of location determination. Updated associated CrewViewWrapper display accordingly.

--- src/components/crews/create/CreateCrewComponent.tsx
+++ src/components/crews/create/CreateCrewComponent.tsx
@@ -1,9 +1,9 @@
+import React, {useState} from "react";
 import {StyledCard} from "../../utilities/cards/StyledCard.tsx";
 import {StyledBodyCard} from "../../utilities/cards/StyledBodyCard.tsx";
 import {StyledCardButton} from "../../utilities/cards/StyledCardButton.tsx";
 import {StyledForm} from "../../utilities/forms/StyledForm.tsx";
 import {CrewCreation} from "../../../services/models/crews/CrewCreation.ts";
-import React from "react";
 import ComboBoxField from "../../utilities/forms/ComboBoxField.tsx";
 import styled from "styled-components";
 import TextAreaField from "../../utilities/forms/TextAreaField.tsx";
@@ -20,13 +20,82 @@ const StyledLabel = styled.label`
 
 const ACTIVITY_OPTIONS = ["Mining", "Trading", "Exploration", "Transportation", "Security", "Rescue", "Salvaging", "Piracy", "Other"];
 const CREW_SIZE_OPTIONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-const SYSTEM_OPTIONS = ["Stanton"];
-const PLANETARY_SYSTEM_OPTIONS = ["Microtech", "Hurston", "ArcCorp", "Crusader"];
-const PLANET_MOON_OPTIONS = ["Microtech", "Hurston", "ArcCorp", "Crusader", "Delamar", "Yela", "Daymar", "Cellin", "Aberdeen", "Magda", "Ita", "Calliope", "Clio"];
-const PLACE_OPTIONS = ["New Babbage", "Lorville", "Area 18", "GrimHex", "Port Tressler", "Baijini Point", "Everus Harbor", "seraphim Station"];
 const LANGUAGES_OPTIONS = ["EN", "FR", "DE", "ES", "IT", "PT", "RU", "ZH", "JA", "KO"];
 
+const SYSTEM_OPTIONS = [
+    {
+        name: "Stanton",
+        planetarySystems: [
+            {
+                name: "Microtech",
+                moons: [
+                    {name: "Microtech", places: ["New Babbage", "Port Tressler"]},
+                ]
+            },
+            {
+                name: "Hurston",
+                moons: [
+                    {name: "Hurston", places: ["Lorville", "Everus Harbor"]},
+                ]
+            },
+            {
+                name: "ArcCorp",
+                moons: [
+                    {name: "ArcCorp", places: ["Area 18", "Baijini Point"]},
+                ]
+            },
+            {
+                name: "Crusader",
+                moons: [
+                    {name: "Crusader", places: ["Orison", "Port Olisar"]},
+                    {name: "Yela", places: ["GrimHex"]},
+                ]
+            }
+        ]
+    }
+];
+
 const CreateCrewComponent = () => {
+    const [selectedSystem, setSelectedSystem] = useState(SYSTEM_OPTIONS[0]);
+    const [selectedPlanetarySystem, setSelectedPlanetarySystem] = useState(SYSTEM_OPTIONS[0].planetarySystems[0]);
+    const [selectedMoon, setSelectedMoon] = useState(SYSTEM_OPTIONS[0].planetarySystems[0].moons[0]);
+    const [places, setPlaces] = useState(SYSTEM_OPTIONS[0].planetarySystems[0].moons[0].places);
+    const [selectedPlace, setSelectedPlace] = useState(places[0]);
+
+    const handleSystemChange = (value: string) => {
+        const system = SYSTEM_OPTIONS.find(system => system.name === value);
+        if (system) {
+            setSelectedSystem(system);
+            const firstPlanetarySystem = system.planetarySystems[0];
+            setSelectedPlanetarySystem(firstPlanetarySystem);
+            const firstMoon = firstPlanetarySystem.moons[0];
+            setSelectedMoon(firstMoon);
+            setPlaces(firstMoon.places);
+        }
+    };
+
+    const handlePlanetarySystemChange = (value: string) => {
+        const planetarySystem = selectedSystem.planetarySystems.find(ps => ps.name === value);
+        if (planetarySystem) {
+            setSelectedPlanetarySystem(planetarySystem);
+            const firstMoon = planetarySystem.moons[0];
+            setSelectedMoon(firstMoon);
+            setPlaces(firstMoon.places);
+        }
+    };
+
+    const handleMoonChange = (value: string) => {
+        const moon = selectedPlanetarySystem.moons.find(moon => moon.name === value);
+        if (moon) {
+            setSelectedMoon(moon);
+            setPlaces(moon.places);
+        }
+    };
+
+    const handlePlaceChange = (value: string) => {
+        setSelectedPlace(value);
+    };
+
     const initialCrew: CrewCreation = {
         crewSize: 1,
         system: 'Stanton',
@@ -62,7 +131,7 @@ const CreateCrewComponent = () => {
                     <ComboBoxField inputName="activityName" value={crew.activityName} options={ACTIVITY_OPTIONS}
                                    onChange={(value) => updateCrew("activityName", value)}/>
 
-                    <StyledLabel>Description:</StyledLabel>
+                    <StyledLabel>Description (optional):</StyledLabel>
                     <TextAreaField inputName="description" value={crew.description}
                                    onChange={(value) => updateCrew("description", value)} minLength={10}
                                    maxLength={100} height="8rem" width="100%"/>
@@ -72,21 +141,48 @@ const CreateCrewComponent = () => {
                                    onChange={(value) => updateCrew("crewSize", Number(value))}/>
 
                     <StyledLabel>System:</StyledLabel>
-                    <ComboBoxField inputName="system" value={crew.system} options={SYSTEM_OPTIONS}
-                                   onChange={(value) => updateCrew("system", value)}/>
+                    <ComboBoxField
+                        inputName="system"
+                        value={selectedSystem.name}
+                        options={SYSTEM_OPTIONS.map(system => system.name)}
+                        onChange={(value) => {
+                            updateCrew("system", value);
+                            handleSystemChange(value);
+                        }}
+                    />
 
                     <StyledLabel>Planetary System:</StyledLabel>
-                    <ComboBoxField inputName="planetarySystem" value={crew.planetarySystem}
-                                   options={PLANETARY_SYSTEM_OPTIONS}
-                                   onChange={(value) => updateCrew("planetarySystem", value)}/>
+                    <ComboBoxField
+                        inputName="planetarySystem"
+                        value={selectedPlanetarySystem.name}
+                        options={selectedSystem.planetarySystems.map(ps => ps.name)}
+                        onChange={(value) => {
+                            updateCrew("planetarySystem", value);
+                            handlePlanetarySystemChange(value);
+                        }}
+                    />
 
                     <StyledLabel>Planet/Moon:</StyledLabel>
-                    <ComboBoxField inputName="planetMoon" value={crew.planetMoon} options={PLANET_MOON_OPTIONS}
-                                   onChange={(value) => updateCrew("planetMoon", value)}/>
+                    <ComboBoxField
+                        inputName="moon"
+                        value={selectedMoon.name}
+                        options={selectedPlanetarySystem.moons.map(moon => moon.name)}
+                        onChange={(value) => {
+                            updateCrew("planetMoon", value);
+                            handleMoonChange(value);
+                        }}
+                    />
 
                     <StyledLabel>Place:</StyledLabel>
-                    <ComboBoxField inputName="place" value={crew.place} options={PLACE_OPTIONS}
-                                   onChange={(value) => updateCrew("place", value)}/>
+                    <ComboBoxField
+                        inputName="place"
+                        value={selectedPlace}
+                        options={places}
+                        onChange={(value) => {
+                            updateCrew("place", value);
+                            handlePlaceChange(value);
+                        }}
+                    />
 
                     <StyledLabel>Languages:</StyledLabel>
                     <MultiComboBoxField inputName="languagesAbbrevs" value={crew.languagesAbbrevs}
@@ -94,7 +190,8 @@ const CreateCrewComponent = () => {
                                         onChange={(value) => updateCrew("languagesAbbrevs", value)}/>
                 </StyledBodyCard>
 
-                <StyledCardButton type="submit" $buttonBackgroundColor={"green"} $canClick={true}>Create</StyledCardButton>
+                <StyledCardButton type="submit" $buttonBackgroundColor={"green"}
+                                  $canClick={true}>Create</StyledCardButton>
             </StyledForm>
         </StyledCard>
     );
