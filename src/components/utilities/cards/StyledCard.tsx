5b14b3cd991f2f3ee6bda7657f9eed6b48b40322
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-17 10:58:55 -0600

    Update media queries for StyledCard component
    The media queries within the StyledCard component have been revised for better responsive design. The min-width values have been modified in two media queries at breakpoints of 350px and 400px, ensuring better layout adjustment on different screen sizes. The 'width: 100%' attribute has also been removed to refine the sizing approach of the cards.

--- src/components/utilities/cards/StyledCard.tsx
+++ src/components/utilities/cards/StyledCard.tsx
@@ -12,14 +12,17 @@ export const StyledCard = styled.div<StyledCardProps>`
     flex-direction: column;
     background-color: ${colors.primary};
     border-radius: 1rem;
-    width: 100%;
     min-width: 25rem;
     max-width: ${props => props.$maxWidth || '25rem'};
     min-height: ${props => props.$minHeight || '30rem'};
     max-height: ${props => props.$maxHeight || '90rem'};
     flex-shrink: 0;
 
-    @media (max-width: 400px) {
-        min-width: 4rem;
+    @media (min-width: 350px) {
+        min-width: 20rem;
+    }
+
+    @media (max-width: 350px) {
+        min-width: 15rem;
     }
 `;
