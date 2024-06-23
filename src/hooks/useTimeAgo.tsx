ece512e12ff2b27e6bd9819a5208f294f2d658bb
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-23 15:40:01 -0600

    Update time calculation in useTimeAgo hook
    The current timestamp is now being generated using new Date().toISOString() instead of just new Date(). This ensures that the timestamp is consistent with the Coordinated Universal Time (UTC), which helps to prevent potential time discrepancies between different time zones.

--- src/hooks/useTimeAgo.tsx
+++ src/hooks/useTimeAgo.tsx
@@ -4,22 +4,22 @@ const useTimeAgo = (date: Date) => {
     const [timeAgo, setTimeAgo] = useState('');
 
     useEffect(() => {
-            const now = new Date();
-            const differenceInMilliseconds = now.getTime() - date.getTime();
-            const differenceInSeconds = Math.floor(differenceInMilliseconds / 1000);
-            const differenceInMinutes = Math.floor(differenceInSeconds / 60);
-            const differenceInHours = Math.floor(differenceInMinutes / 60);
-            const differenceInDays = Math.floor(differenceInHours / 24);
+        const now = new Date(new Date().toISOString());
+        const differenceInMilliseconds = now.getTime() - date.getTime();
+        const differenceInSeconds = Math.floor(differenceInMilliseconds / 1000);
+        const differenceInMinutes = Math.floor(differenceInSeconds / 60);
+        const differenceInHours = Math.floor(differenceInMinutes / 60);
+        const differenceInDays = Math.floor(differenceInHours / 24);
 
-            if (differenceInDays > 0) {
-                setTimeAgo(`${differenceInDays} days ago`);
-            } else if (differenceInHours > 0) {
-                setTimeAgo(`${differenceInHours} hours ago`);
-            } else if (differenceInMinutes > 0) {
-                setTimeAgo(`${differenceInMinutes} minutes ago`);
-            } else {
-                setTimeAgo(`${differenceInSeconds} seconds ago`);
-            }
+        if (differenceInDays > 0) {
+            setTimeAgo(`${differenceInDays} days ago`);
+        } else if (differenceInHours > 0) {
+            setTimeAgo(`${differenceInHours} hours ago`);
+        } else if (differenceInMinutes > 0) {
+            setTimeAgo(`${differenceInMinutes} minutes ago`);
+        } else {
+            setTimeAgo(`${differenceInSeconds} seconds ago`);
+        }
     }, [date]);
 
     return timeAgo;
