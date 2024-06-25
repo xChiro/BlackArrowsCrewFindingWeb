350a18f768ff37fed9357ff331bbf64857a4eac4
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-24 21:46:44 -0600

    Add 'ignore-cache' plugin to Vite config
    This commit includes an additional plugin in the Vite configuration to ignore any files with '.cache' in their name. This plugin is designed to prevent unnecessary processing of cache files during build time.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), {
+        name: 'ignore-cache',
+        load(id) {
+            if (id.includes('.cache')) {
+                return null;
+            }
+        }
+    }],
 })
