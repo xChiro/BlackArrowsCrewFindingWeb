fa33fbd7e137d656fb6756930a520980a1b0eec7
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-25 12:10:10 -0600

    Add social media links to footer
    Two social media links, Discord and Reddit, have been added to the footer component. The links are provided through FontAwesome icons. A new styled component, SocialMediaDiv, has been created to manage the display and behavior of these links.

--- src/components/footer/FooterComponent.tsx
+++ src/components/footer/FooterComponent.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import {colors} from "../../themes/Colors.ts";
 import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDiscord, faReddit} from "@fortawesome/free-brands-svg-icons";
 
 const StyledFooter = styled.footer`
     position: fixed;
@@ -22,9 +24,36 @@ const StyledFooter = styled.footer`
     }
 `;
 
+const SocialMediaDiv = styled.div`
+    display: flex;
+    gap: .5rem;
+    text-align: right;
+    margin-top: .5rem;
+
+    a {
+        color: white;
+    }
+
+    span {
+        font-size: .8rem;
+    }
+
+    @media (max-width: 600px) {
+        justify-content: center;
+        text-align: center;
+    }
+`;
+
 const LeftAlignedDiv = styled.div`
     text-align: left;
     margin: 0 .5rem;
+    
+    @media (max-width: 600px) {
+        flex-direction: row;
+        align-items: center;
+        text-align: center;
+        align-content: center;
+    }
 `;
 
 const CenterAlignedDiv = styled.div`
@@ -50,6 +79,15 @@ const Footer = () => {
         <StyledFooter>
             <LeftAlignedDiv>
                 <StyledLink to="/">SC Crew Finding by Black Arrows</StyledLink>
+                <SocialMediaDiv>
+                    <span>Join to us:</span>
+                    <a href="https://www.reddit.com/r/StarCitizen_Crews/">
+                        <FontAwesomeIcon icon={faReddit} />
+                    </a>
+                    <a href="https://discord.gg/K74phC2Kfh">
+                        <FontAwesomeIcon icon={faDiscord} />
+                    </a>
+                </SocialMediaDiv>
             </LeftAlignedDiv>
             <CenterAlignedDiv>
                 <span style={{fontSize: ".8rem"}}>
