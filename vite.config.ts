df70a04eddd7eb41554b1b2ba6c0041945809f3f
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-24 21:58:54 -0600

    Update vite configuration and remove unused SVG asset
    This commit introduces changes to the configuration of the vite build tool. It specifies rollup options for build inputs and outputs whilst also maintaining the earlier functionality to ignore cache during load. Besides, it removes an unused react SVG asset from the project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), {
-        name: 'ignore-cache',
-        load(id) {
-            if (id.includes('.cache')) {
-                return null;
-            }
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html'
+            },
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            },
+            plugins: [react(), {
+                name: 'ignore-cache',
+                load(id) {
+                    if (id.includes('.cache')) {
+                        return null;
+                    }
+                }
+            }],
         }
-    }],
+    }
 })
