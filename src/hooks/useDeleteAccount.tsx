f48bc4dc8207009ea2213b9ba31e044234c500a0
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-07-01 11:23:12 -0600

    Update useDeleteAccount hook
    The useDeleteAccount hook has been updated to use a reference to PlayerService instead of recreating it on every deletion request. This significantly reduces the overhead of generating new instances. Also, the useEffect hook was used to ensure the current PlayerService instance is updated when the access token changes.

--- src/hooks/useDeleteAccount.tsx
+++ src/hooks/useDeleteAccount.tsx
@@ -1,23 +1,28 @@
 import useYesNoModal from "./modals/useYesNoModal.tsx";
-import {useCallback} from "react";
+import {useCallback, useEffect, useRef} from "react";
 import {useAuth} from "./useAuth.tsx";
 import PlayerService from "../services/PlayerService.ts";
 
 export const useDeleteAccount = () => {
     const {getAccessToken, logout} = useAuth();
+    const playerServiceRef = useRef<PlayerService>(new PlayerService(getAccessToken()));
+
+    useEffect(() => {
+        const token = getAccessToken();
+        playerServiceRef.current = new PlayerService(token);
+    }, [getAccessToken]);
 
     const onDeleteAccount = useCallback(() => {
-        const playerService = new PlayerService(getAccessToken());
-        playerService.deleteAccount().then(() => {
+        playerServiceRef.current.deleteAccount().then(() => {
             logout();
         });
-    }, [getAccessToken, logout]);
+    }, [logout]);
 
-    const { openModal, ModalComponent } = useYesNoModal("Are you sure you want to delete your account?", onDeleteAccount);
+    const {openModal, ModalComponent} = useYesNoModal("Are you sure you want to delete your account?", onDeleteAccount);
 
     const deleteAccount = useCallback(() => {
         openModal();
     }, [openModal]);
 
-    return { deleteAccount, ModalComponent };
+    return {deleteAccount, ModalComponent};
 };
